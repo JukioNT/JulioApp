9bc691c4cdedd9d9a80d0d08a90fb20317689c68
JukioNT <jjulio2468@gmail.com>
2023-06-05 10:18:53 -0300

    Map & marker

--- src/navigations/tab.navigation.tsx
+++ src/navigations/tab.navigation.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {  BottomTabNavigationProp, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { ScreenPerfil, ScreenCamera } from "../screens";
+import { ScreenPerfil, ScreenCamera, ScreenLocation } from "../screens";
 import { colors } from '../styles/colors';
-import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { Ionicons, AntDesign, Entypo } from '@expo/vector-icons';
 import { CameraNavigation } from './camera.navigation';
 type TabParamList = {
   Perfil: undefined;
-  Camera: undefined
+  Camera: undefined;
+  Location: undefined 
 };
 
 type TabScreenNavigationProp = BottomTabNavigationProp<TabParamList, 'Perfil'>
@@ -29,13 +30,19 @@ export function TabNavigation() {
     >
       <Tab.Screen name="Perfil" component={ScreenPerfil} 
         options={{
-          tabBarIcon: () => (<Ionicons name='person' color={colors.white} size={24}/>)
+          tabBarIcon: () => (<Ionicons name='person' color="black000000000
+          " size={24}/>)
         }}
       
       />
       <Tab.Screen name='Camera' component={CameraNavigation}
         options={{
-          tabBarIcon: () => (<AntDesign name='camera' color={colors.white} size={24}/>)
+          tabBarIcon: () => (<AntDesign name='camera' color="black" size={24}/>)
+        }}
+      />
+      <Tab.Screen name='Location' component={ScreenLocation}
+        options={{
+          tabBarIcon: () => (<Entypo name="map" size={24} color="black" />)
         }}
       />
     </Tab.Navigator>
